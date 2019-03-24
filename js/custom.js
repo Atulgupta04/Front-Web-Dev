4c411f3e9e765c6b151d983ab3f46335c1d819fc
Atul Gupta <atulgupta.kr4@gmail.com>
2019-03-24 18:09:11 +0530

    added our work section

--- js/custom.js
+++ js/custom.js
@@ -3,3 +3,15 @@ $(function() {
 	//animate on scroll
 	new WOW().init();
 });
+
+// ============Work============//
+
+$(function(){
+	$('#work').magnificPopup({
+		delegate: 'a', //child items selector, by clicking on it popup will open
+		type: 'image',
+		gallery : {
+			enabled: true
+		}
+	});
+});
